a330ae1a516532fa700fa1ceeb36e95dcf125283
imranhasan2 <imranhasan030303@gmail.com>
2024-05-02 22:58:28 +0600

    updated env file

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,13 +6,17 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC2UuBFumAoEk5rLx-Sfxx7Pk0Ay1xOu_M",
-  authDomain: "mintie-art-and-craft.firebaseapp.com",
-  projectId: "mintie-art-and-craft",
-  storageBucket: "mintie-art-and-craft.appspot.com",
-  messagingSenderId: "252334999144",
-  appId: "1:252334999144:web:54fea00509d2bc81d15ddd"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAIN,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
